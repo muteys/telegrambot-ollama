4163c18039949e04ff760458b9bf0dd737b12e7f
Ilya Larin <1hersil88@gmail.com>
2025-01-31 19:02:31 +0600

    nothing_happend

---
+++ ai_test.py
@@ -0,0 +1,18 @@
+import pathlib
+import textwrap
+import os
+import google.generativeai as genai
+
+from IPython.display import display
+from IPython.display import Markdown
+
+def to_markdown(text):
+  text = text.replace('•', '  *')
+  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+
+GOOGLE_API_KEY=os.getenv('AI_TOKEN')
+genai.configure(api_key=GOOGLE_API_KEY)
+
+for m in genai.list_models():
+  if 'generateContent' in m.supported_generation_methods:
+    print(m.name)
